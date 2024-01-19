1c68ebb4a10b6bc62e5be124e9c3b3c6475fde8c
baya laadjel <bata_cher@hotmail.fr>
2024-01-19 11:00:49 +0100

    supp header

--- Application/src/pages/EmployeeList.jsx
+++ Application/src/pages/EmployeeList.jsx
@@ -6,12 +6,6 @@ import EmployeeTable from '../components/EmployeeTable';
 export default function EmployeeList() {
   return (
     <>
-      <header className='bg-emerald-50 p-2'>
-        <Link to='/' className='flex flex-row gap-3 items-center justify-center'>
-          <img  width="50" height="50" src={ImgLogo} alt="Logo HRNet" />
-          <p> HRNet</p>          
-        </Link>
-      </header>
       <EmployeeTable />  
     </>
   )
