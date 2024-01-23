7702dc7e2c18b5b83128dcff4e56f40ac3c8810a
baya laadjel <bata_cher@hotmail.fr>
2024-01-23 13:31:25 +0100

    modif couleur

--- Application/src/pages/Home.jsx
+++ Application/src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className='mx-16 my-6 '>
         <div className='flex justify-between gap-6 mx-10 my-6 text-xl font-semibold'>
           <h2>Create Employee</h2>
-          <NavLink to='/EmployeeList' className='text-emerald-600 underline underline-offset-4'> View Current  Employees</NavLink>     
+          <NavLink to='/EmployeeList' className='text-emerald-800 underline underline-offset-4'> View Current  Employees</NavLink>     
         </div>
         <HRnetForm />
       </div>
