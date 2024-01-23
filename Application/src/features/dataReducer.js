58c143c78be60343984922c4bb2c87cc9cc0ab54
baya laadjel <bata_cher@hotmail.fr>
2024-01-23 11:33:23 +0100

    add redux-persist

--- Application/src/features/dataReducer.js
+++ Application/src/features/dataReducer.js
@@ -1,11 +1,5 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import {createSlice} from '@reduxjs/toolkit'
 import DataEmployee from '../../data/MOCK_DATA.json'
-
-// export const getEmploys = createAsyncThunk('data/getEmploys', async()=>{//créer une réflexion asynchrone
-//     const res = await fetch('../../data/MOCK_DATA.json')
-//     const DataEmployee = await res.json()
-//     return  DataEmployee
-// })
 
 export const dataSlice = createSlice({
     name: 'data',
@@ -15,18 +9,6 @@ export const dataSlice = createSlice({
             state.employs.push(action.payload)
         }
     },  
-    // extraReducers:(builder) =>{
-    //     builder.addCase(getEmploys.pending,(state, action)=>{
-    //         // console.log(action)
-    //     })
-    //     builder.addCase(getEmploys.fulfilled,(state, action)=>{
-    //         //console.log(action)//contient le type en fullfield et les données en payload
-    //         state.employs = action.payload
-    //     })
-    //     builder.addCase(getEmploys.rejected,(state,action)=>{
-    //         console.log(action)
-    //     })
-    // },
 })
 export const {AddEmploys} = dataSlice.actions
 export default dataSlice.reducer;
--- Application/src/features/store.js
+++ Application/src/features/store.js
@@ -1,8 +1,41 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import dataReducer from './dataReducer'
+import { persistStore, persistReducer,
+         FLUSH,
+         REHYDRATE,
+         PAUSE,
+         PERSIST,
+         PURGE,
+         REGISTER } from 'redux-persist'
+import localStorage  from 'redux-persist/lib/storage'
 
+/**
+ * Configuration of Redux Persist
+ */
+const persistConfig = {
+    key: 'root',
+    storage: localStorage,
+}
+/**
+ * Creation of reducers
+ */
+const rootReducer = combineReducers ({
+    data : dataReducer,
+  })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+/**
+ * Store configuration
+ */
 export const store = configureStore({
-    reducer:{
-        data : dataReducer,
-    }
+    reducer : persistedReducer ,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+export const persistor = persistStore(store)
