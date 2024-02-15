eded039dd7ac41680da3b93233af714af0832016
baya laadjel <bata_cher@hotmail.fr>
2024-02-15 15:12:15 +0100

    delete employeeList page + add Header o App

--- Application/src/App.jsx
+++ Application/src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
-import EmployeeList from './pages/EmployeeList';
 import { Provider } from 'react-redux'
 import { store } from "./features/store";
+import Header from './components/Header.jsx'
+import EmployeeTable from './components/EmployeeTable.jsx';
 
 
 function App() {
-  return (
-    <Provider store={store}>
+  return ( 
+    <Provider store={store}>  
      <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/EmployeeList" element ={<EmployeeList />} />
+          <Route path="/EmployeeTable" element ={<EmployeeTable />} />
         </Routes>
       </Router>
     </Provider>
--- Application/src/pages/Home.jsx
+++ Application/src/pages/Home.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { HRnetForm } from '../components/HRnetForm.jsx';
 import { NavLink } from 'react-router-dom';
-import Header from '../components/Header.jsx';
+import EmployeeTable from '../components/EmployeeTable.jsx';
 
 export default function Home() {
   return (
-    <> 
-      <Header />
+    <>     
       <div className='mx-16 my-6 '>
         <div className='flex justify-between gap-6 mx-10 my-6 text-xl font-semibold'>
           <h2>Create Employee</h2>
-          <NavLink to='/EmployeeList' className='text-emerald-800 underline underline-offset-4'> View Current  Employees</NavLink>     
+          <NavLink to='/EmployeeTable' className='text-emerald-800 underline underline-offset-4'> View Current  Employees</NavLink>     
         </div>
         <HRnetForm />
       </div>
